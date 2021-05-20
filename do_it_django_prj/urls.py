cee33d5aab1d5c0e4460bcf27da3d59bb37df36a
jacob <gktjd1253@naver.com>
2021-05-20 18:38:37 +0900

    대문 페이지와 자기소개 페이지 만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('', include('single_pages.urls')),
 
 
 ]
